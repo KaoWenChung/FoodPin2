c959b37bdac9de0e8fb0f2d7f8cf4b1c96bc8542
wyn <k022298@gmail.com>
2020-04-22 16:03:37 +0800

    add starting view

---
+++ FoodPin/Controller/WalkThroughContentViewController.swift
@@ -0,0 +1,37 @@
+//
+//  WalkThroughContentViewController.swift
+//  FoodPin
+//
+//  Created by wyn on 2020/4/22.
+//  Copyright © 2020 Wyn. All rights reserved.
+//
+
+import UIKit
+
+class WalkThroughContentViewController: UIViewController {
+    
+    @IBOutlet var headingLabel: UILabel! {
+        didSet {
+            headingLabel.numberOfLines = 0
+        }
+    }
+    @IBOutlet var subHeadingLabel: UILabel! {
+        didSet {
+            subHeadingLabel.numberOfLines = 0
+        }
+    }
+    @IBOutlet var contentImageView: UIImageView!
+    
+    var index = 0
+    var heading = ""
+    var subHeading = ""
+    var imageFile = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        headingLabel.text = heading
+        subHeadingLabel.text = subHeading
+        contentImageView.image = UIImage(named: imageFile)
+    }
+}
---
+++ FoodPin/Controller/WalkThroughPageViewController.swift
@@ -0,0 +1,67 @@
+//
+//  WalkThroughPageViewController.swift
+//  FoodPin
+//
+//  Created by wyn on 2020/4/22.
+//  Copyright © 2020 Wyn. All rights reserved.
+//
+
+import UIKit
+
+class WalkThroughPageViewController: UIPageViewController, UIPageViewControllerDataSource {
+    
+    var pageHeadings = ["CREATE YOUR OWN FOOD GUIDE", "SHOW YOU THE LOCATION", "DISCOVER GREAT RESTAURANTS"]
+    var pageImages = ["onboarding-1", "onboarding-2", "onboarding-3"]
+    var pageSubHeadings = ["Pin your favorite restaurants and create your own food guide",
+                           "Search and locate your favorite restaurant on Maps",
+                           "Find restaurants shared by your friends and other foodies"]
+    var currentIndex = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set data soucrce to self
+        dataSource = self
+        
+        // Create the first starting view controller
+        if let startingViewController = contentViewController(at: 0) {
+            setViewControllers([startingViewController], direction: .forward, animated: true, completion: nil)
+        }
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        var index = (viewController as! WalkThroughContentViewController).index
+        index -= 1
+        
+        return contentViewController(at: index)
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        var index = (viewController as! WalkThroughContentViewController).index
+        index += 1
+        
+        return contentViewController(at: index)
+    }
+    
+    func contentViewController(at index: Int) -> WalkThroughContentViewController? {
+        if index < 0 || index >= pageHeadings.count {
+            return nil
+        }
+
+        // 建立新的視圖控制器並傳遞適合的資料
+        let storyboard = UIStoryboard(name: "Onboarding", bundle: nil)
+        if let pageContentViewController = storyboard.instantiateViewController(withIdentifier: "WalkThroughContentViewController") as? WalkThroughContentViewController {
+
+            pageContentViewController.imageFile = pageImages[index]
+            pageContentViewController.heading = pageHeadings[index]
+            pageContentViewController.subHeading = pageSubHeadings[index]
+            pageContentViewController.index = index
+
+            return pageContentViewController
+        }
+
+        return nil
+    }
+}
